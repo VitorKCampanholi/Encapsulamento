7210a3936498eebfc214ef98c491b45291e4fce8
VKBOOY <VKBOOY@DESKTOP-DE9CVF9>
2024-11-14 11:28:03 -0300

    Adicionar arquivos de projeto.

---
+++ Encapsulamento/Produto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    internal class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+        public Produto()
+        {
+
+        }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+
+        }
+
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)  {
+            if(nome != null && nome.Length > 1)  {
+
+            }
+            _nome = nome;
+        }
+
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+        public void RemoveProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return  
+                _nome + ", $" + 
+                _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " +  
+                _quantidade 
+                + " unidades Total: $" 
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
+
+
+
---
+++ Encapsulamento/Program.cs
@@ -0,0 +1,11 @@
+﻿using Encapsulamento;
+using System;
+using System.Globalization;
+
+Produto p = new Produto("TV", 500.00, 10);
+
+p.SetNome("T");
+
+Console.WriteLine(p.GetNome());
+Console.WriteLine(p.GetPreco());
+ 
